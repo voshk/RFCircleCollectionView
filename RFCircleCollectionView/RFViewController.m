0fca96ae30ac1b58d7fed6e6a01b2876598e0cf8
sensejump <sensejump@foxmail.com>
2017-05-17 18:35:39 +0800

    bug fix

--- RFCircleCollectionView/RFViewController.m
+++ RFCircleCollectionView/RFViewController.m
@@ -5,7 +5,7 @@
 //
 //  Created by Arvin on 15/11/25.
 //  Copyright © 2015年 mobi.refine. All rights reserved.
-//
+//  https://github.com/sensejump/RFCircleCollectionView
 
 #import "RFViewController.h"
 #import "RFCollectionViewCell.h"
